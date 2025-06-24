6e11cd2a5005678030ff74aa5eacc15083a489a8
rejj0925 <150020298+rejj0925@users.noreply.github.com>
2025-06-24 14:48:24 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import LoginPage from './LoginPage'; 
+import './App.css';
+import LoginPage from './LoginPage';
+import LandingPage from './LandingPage'
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-      <LoginPage />
-    </div>
-  );
-}
-
-export default App;
+    <Routes>
+        <Route path="/" element={<LoginPage />}/>
+        <Route path="/LandingPage" element={<LandingPage />}/>
+    </Routes>
